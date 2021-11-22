22c3d0a752e13be5815f2c68791a9cbce4730a94
thebongy <rishit.bansal0@gmail.com>
2021-11-22 15:10:24 +0530

    feat: Add activity for custom meetings

---
+++ app/src/main/java/com/dyte/kotlinsample/CustomMeetingActivity.kt
@@ -0,0 +1,11 @@
+package com.dyte.kotlinsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomMeetingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_meeting)
+    }
+}
